cc5c8c92d38456d48783606960e17f31343a7c13
Rakhi Patil <rakhipatil5162@gmail.com>
2025-12-11 00:26:37 +0530

    add subtract calc_rect_area calc_square_area

---
+++ geometric.py
@@ -0,0 +1,5 @@
+def calc_rect_peri(l,b):
+    print("Perimeter of rectangle is: ",(2*(l+b)) )
+
+def calc_square_peri(s):
+    print("Perimeter of square is: ",(s*s))
--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-
+import arithmetic
+import geometric 
 print("Hello, World!")
+num1 = int(input("Enter num1: "))
+num2 = int(input("Enter num2: "))
+
+arithmetic.add(num1,num2)
+arithmetic.subtract(num1,num2)
+geometric.calc_rect_peri(3,2)
+geometric.calc_square_peri(4)
